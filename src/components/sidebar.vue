82803d197ee725ad6f66cd6d3565d1cdc8b76c3c
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-10-03 18:23:55 +0545

    bug bug bug

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -8,9 +8,7 @@
       <div class="pt-10 space-y-10">
         <div class="inline-flex hover:bg-green cursor-pointer p-2 rounded-full">
           <i class="fa-solid fa-house text-white text-4xl"></i>
-          <h1 class="hidden md:flex px-5 pt-2 hover:text-xl text-white">
-            Home
-          </h1>
+          <h1 class="hidden md:flex px-5 pt-2 text-xl text-white">Home</h1>
         </div>
         <div class="inline-flex hover:bg-green cursor-pointer p-2 rounded-full">
           <i class="fa-solid fa-address-card text-white text-4xl"></i>
@@ -31,18 +29,18 @@
           <h1 class="hidden md:flex px-5 pt-2 text-xl text-white">Contact</h1>
         </div>
       </div>
-      <div class="px-3 mt-10">
+      <div class="px-3 mt-10 md:space-x-5 space-y-5">
         <i class="fa-brands fa-facebook text-white text-xl hover:text-3xl"></i>
         <i class="fa-brands fa-github text-white text-xl hover:text-3xl"></i>
         <i class="fa-brands fa-instagram text-white text-xl hover:text-3xl"></i>
       </div>
 
-      <div>
-        <div class="mt-10 m-auto p-0 flex space-x-0.5">
+      <div class="my-10">
+        <div class="hidden md:inline-flex">
           <i class="fa-regular fa-copyright text-blue-500 py-1"></i>
           <h1 class="text-blue-500">All right reserved.</h1>
         </div>
-        <div class="text-white p-3">Bibek Timilsina</div>
+        <div class="hidden md:flex text-white p-3">Bibek Timilsina</div>
       </div>
     </div>
   </div>
